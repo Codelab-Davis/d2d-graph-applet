57822d62b1f0771e8e53f55e33cb2daca5e78d63
alexisbl14 <ablydon@ucdavis.edu>
2023-10-30 19:51:10 -0700

    finished parsing for time stamps and cells

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   const [sum, setSum] = useState(0);
 
   const click = () => {
+    // temp was for adding variables to set to the sum
     let temp = 0; 
     
     fetch(`https://api.fureweb.com/spreadsheets/${sheetId}`, {
@@ -16,9 +17,18 @@ function App() {
       .then(response => response.json())
       .then(data => {
         for (let count = 0; count < data.data.length; count++) {
-          temp += data.data[count]['a'];
+          //temp += data.data[count]['a'];
+          let timeObject = data.data[count]['Time'];
+          timeObject = timeObject.split(",");
           
-          console.log(data.data[count]['a']);
+          // print minutes
+          console.log(timeObject[4]);
+
+          // print seconds, get rid of trailing parenthesis
+          console.log(timeObject[5].substring(0, timeObject[5].length - 1));
+
+          // print cell A1, repeat for other cells
+          console.log(data.data[count]['A1']);
         }
         setSum(temp);
         console.log(temp);
@@ -26,6 +36,7 @@ function App() {
       .catch(error => console.error(error))
     return temp;
   }
+  // dummy variables
   // const spreadsheetId = '10WDbAPAY7Xl5DT36VuMheTPTTpqx9x0C5sDCnh4BGps'
 
   const change =  (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,6 +46,7 @@ function App() {
 
   return (
     <div className = "App">
+      <h1>D2D</h1>
       <input onChange = {change}
       value = {sheetId}/>
       <button onClick =  {click}> sum is {sum}
