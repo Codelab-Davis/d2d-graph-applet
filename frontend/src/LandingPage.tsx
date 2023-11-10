ceb08c1486c4608baa115e9d7ec37312b7860506
alexisbl14 <ablydon@ucdavis.edu>
2023-11-09 18:07:04 -0800

    added design configurations and cleaned up styling

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -3,20 +3,19 @@ function LandingPage() {
         console.log("You clicked!")
     }
     return (
-        <div className='flex justify-center items-center h-screen bg-contain bg-center bg-landing-page'>
-            <div className="flex self-center flex-col w-[70%]">
-                <div className="flex my-14 h-fit w-fit justify-start">
-                    <img src='./src/assets/d2dlogo.png' className='w-[153.24px] ml-6'></img>                   
-                    {/*<p className='font-semibold font-inter self-center text-[22.89px]/[29.76px]'>Design2Data</p>*/}
+        <div className='flex justify-center items-center bg-cover bg-center bg-landing-page h-screen'>
+            <div className="flex flex-col self-center w-[70%]">
+                <div className="flex justify-start my-14 h-fit w-fit">
+                    <img src='./src/assets/d2dlogo.png' className='object-contain ml-6 w-[153px]'></img>
                 </div>
-                <div className="flex flex-col items-center justify-between bg-white rounded-3xl py-[163px] mb-44">
-                    <h1 className="text-grays-700 mb-[54px]">ENZYME RATE CALCULATOR</h1>
-                    <div className="flex justify-between items-center h-[67px] w-[516px] rounded-[40px] mb-[36px] border-2 border-grays-400 p-[4px]">
-                        <input className="grow pl-[20px] font-manrope font-medium text-lg placeholder-grays-600 focus:outline-none" placeholder='Paste URL'></input>
-                        <button onClick={click} className="bg-secondary-500 px-[20px] mx-[6px] h-[47px] rounded-[30px] text-white text-lg font-semibold font-manrope">Calculate</button>
+                <div className="flex flex-col justify-between items-center mb-44 py-[163px] bg-white rounded-3xl">
+                    <h1 className="mb-[54px] text-grays-700">ENZYME RATE CALCULATOR</h1>
+                    <div className="flex justify-between items-center border-2  border-grays-400 mb-[36px] p-[4px] h-[67px] w-[516px] rounded-[40px]">
+                        <input className="grow pl-[20px] font-manrope font-medium text-base placeholder-grays-600 focus:outline-none" placeholder='Paste URL'></input>
+                        <button onClick={click} className="mx-[6px] px-[20px] h-[47px] bg-secondary-500 rounded-[30px] text-base font-semibold font-manrope text-white">Calculate</button>
                     </div>
                     <div className="flex justify-center gap-2">
-                        <a className="underline text-grays-700 font-manrope font-medium text-[18px]" href="https://www.youtube.com/">How it works</a>
+                        <a className="text-base font-manrope font-medium underline text-grays-700" href="https://www.youtube.com/">How it works</a>
                         <img src="./src/assets/orangeButton.svg"></img>
                     </div>                   
                 </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xs: '0.75rem',
+      sm: '1rem',
+      base: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
