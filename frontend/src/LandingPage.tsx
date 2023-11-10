6256c9489f0dfbef4e552374c0f7799439834fdc
Mohnish2004 <mohnishgopi@gmail.com>
2023-11-10 00:40:45 -0800

    Update LandingPage.tsx

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -10,6 +10,7 @@ function LandingPage() {
                 </div>
                 <div className="flex flex-col justify-between items-center mb-44 py-[163px] bg-white rounded-3xl">
                     <h1 className="mb-[54px] text-grays-700">ENZYME RATE CALCULATOR</h1>
+                    <h3 className="mb-[10px] text-grays-700">Please insert a valid spreadsheet URL</h3>
                     <div className="flex justify-between items-center border-2  border-grays-400 mb-[36px] p-[4px] h-[67px] w-[516px] rounded-[40px]">
                         <input className="grow pl-[20px] font-manrope font-medium text-base placeholder-grays-600 focus:outline-none" placeholder='Paste URL'></input>
                         <button onClick={click} className="mx-[6px] px-[20px] h-[47px] bg-secondary-500 rounded-[30px] text-base font-semibold font-manrope text-white">Calculate</button>
