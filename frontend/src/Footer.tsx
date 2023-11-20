37ca3e80bbd6706c0d3570ba3c2de8b60cebbf04
alexisbl14 <ablydon@ucdavis.edu>
2023-11-20 14:32:34 -0800

    edited width of footer to screen width

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <>
         <footer className="bg-white dark:bg-gray-900">
-          <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+          <div className="mx-auto w-full p-4 py-6 lg:py-8">
             <div className="md:flex md:justify-between">
               <div className="mb-6 md:mb-0">
                 <a href="https://d2d.ucdavis.edu/" className="flex sm:mx-auto items-center">
