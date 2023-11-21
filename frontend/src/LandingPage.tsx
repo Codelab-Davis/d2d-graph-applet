327c39a490f106c169a634a6e5755094f37d605c
alexisbl14 <ablydon@ucdavis.edu>
2023-11-20 18:40:18 -0800

    changed exponential to precision

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -40,7 +40,7 @@ function getData(data:[]){
     
     tempSlope = findSlope(window);
     maxSlope = Math.max(maxSlope, tempSlope);
-    return maxSlope.toExponential(4);
+    return maxSlope.toPrecision(4);
   }
   
 function findSlope(points:number[][]) {
