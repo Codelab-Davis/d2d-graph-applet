9e07d00f720cef0dd2533e38f34a71b95d51b7e6
Mohnish2004 <mohnishgopi@gmail.com>
2023-12-21 20:03:45 +0400

    Minor fixes/ changes
    updated footer links, feedback form & updated operating manual

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -12,11 +12,11 @@ const Footer = () => {
               </div>
               <div className="flex-col flex mt-4 sm:justify-center sm:mt-0 items-center space-y-4 md:space-y-0 md:flex-row md:space-x-8 justify-end">
                 <div className="flex-col flex mt-4 sm:justify-center sm:mt-0 items-center space-y-4 md:space-y-0 md:flex-row md:space-x-8 justify-end">
-                <a href="/public/Operatingmanual.pdf" target = "_blank" className="flex items-center">
+                <a href="/public/Operating_Manual.pdf" target = "_blank" className="flex items-center">
                   <button type="button" className="text-white bg-secondary-600 hover:bg-secondary-700 font-semibold rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-secondary-600 dark:hover:bg-secondary-600 dark:focus:secondary-600">DOCUMENTATION</button>
                   </a>
-                  <a href="" className="flex items-center">
-                  <button type="button" className="py-2.5 px-5 me-2 mb-2 text-base font-semibold text-gray-900 bg-white rounded-full border border-grays-300 hover:bg-grays-300 focus:z-10 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-secondary-600">TRY A DEMO</button>
+                  <a href="https://fpgojzvzwng.typeform.com/to/wegBSZep" className="flex items-center">
+                  <button type="button" className="py-2.5 px-5 me-2 mb-2 text-base font-semibold text-gray-900 bg-white rounded-full border border-grays-300 hover:bg-grays-300 focus:z-10 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-secondary-600">Feedback form</button>
                 </a>
                 </div>
               </div>
@@ -34,11 +34,10 @@ const Footer = () => {
                 <div className="w-[92px] h-[17px] bg-contain bg-no-repeat bg-[url('/assets/codelabLogo.svg')] dark:bg-[url('/assets/codelabLogoWhite.svg')] dark:h-[24px] dark:bg-cover"></div>
               </div>
               <div className="flex gap-3 lg:gap-[40px] mt-4 sm:justify-center sm:mt-0">
-                <a href="https://d2d.ucdavis.edu/" className="hover:underline text-grays-700 dark:text-[#f2f2f2]" >Contact Us</a>
-                <a href="#" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">About D2D</a>
-                <a href="#" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">Privacy Policy</a>
-                <a href="#" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">Terms of Service</a>
-                <a href="#" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">Cookies</a>
+                <a href="https://d2d.ucdavis.edu/contact-us" target="_blank" className="hover:underline text-grays-700 dark:text-[#f2f2f2]" >Contact Us</a>
+                <a href="https://d2d.ucdavis.edu/" target="_blank" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">About D2D</a>
+                <a href="https://www.d2dcure.com/" target="_blank" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">D2D Database</a>
+                <a href="https://medium.com/@mohnish.gopi/d2d-enzyme-rate-calculator-c1cc87f268f4" target="_blank" className="hover:underline text-grays-700 dark:text-[#f2f2f2]">Behind the Scenes</a>
               </div>
             </div>
           </div>
